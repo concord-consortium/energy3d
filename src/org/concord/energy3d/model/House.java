67971de7be14db2e8a5bdd743669a0d3f04f35d0
snourian <snourian@6e01202a-0783-4428-890a-84243c50cc2b>
2010-01-26 21:15:52 +0000

    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/simulations/Energy3D@21736 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/energy3d/model/House.java
@@ -0,0 +1,35 @@
+package org.concord.energy3d.model;
+
+import java.util.ArrayList;
+
+public class House {
+	private static House instance;
+	private ArrayList<HousePart> parts = new ArrayList<HousePart>();
+
+	public static House getInstance() {
+		if (instance == null)
+			instance = new House();
+		return instance;
+	}
+
+	private House() {
+
+	}
+
+	public boolean add(HousePart e) {
+		return parts.add(e);
+	}
+
+	public void clear() {
+		parts.clear();
+	}
+
+	public boolean remove(Object o) {
+		return parts.remove(o);
+	}
+
+	public int size() {
+		return parts.size();
+	}
+
+}
---
+++ src/org/concord/energy3d/model/Wall.java
@@ -0,0 +1,59 @@
+package org.concord.energy3d.model;
+
+import java.nio.FloatBuffer;
+
+import com.ardor3d.image.Texture;
+import com.ardor3d.image.Image.Format;
+import com.ardor3d.math.Vector3;
+import com.ardor3d.renderer.IndexMode;
+import com.ardor3d.renderer.state.MaterialState;
+import com.ardor3d.renderer.state.TextureState;
+import com.ardor3d.renderer.state.MaterialState.ColorMaterial;
+import com.ardor3d.scenegraph.Mesh;
+import com.ardor3d.util.TextureManager;
+import com.ardor3d.util.geom.BufferUtils;
+
+public class Wall extends HousePart {
+	private static final float WALL_HEIGHT = 0.5f;
+	private FloatBuffer vertexBuffer = BufferUtils.createVector3Buffer(4);
+	private FloatBuffer textureBuffer = BufferUtils.createVector2Buffer(4);
+
+	public Wall() {
+		super(2);
+		Mesh mesh = new Mesh("Wall");
+		root.attachChild(mesh);
+		mesh.getMeshData().setIndexMode(IndexMode.QuadStrip);
+		mesh.getMeshData().setVertexBuffer(vertexBuffer);
+		mesh.getMeshData().setTextureBuffer(textureBuffer, 0);
+
+		// Add a material to the box, to show both vertex color and lighting/shading.
+		final MaterialState ms = new MaterialState();
+		ms.setColorMaterial(ColorMaterial.Diffuse);
+		mesh.setRenderState(ms);
+
+		// Add a texture to the box.
+		final TextureState ts = new TextureState();
+		ts.setTexture(TextureManager.load("brick_wall.jpg", Texture.MinificationFilter.Trilinear, Format.GuessNoCompression, true));
+		mesh.setRenderState(ts);
+	}
+
+	@Override
+	protected void draw(Vector3 p, int i) {
+		vertexBuffer.position(i * 2 * 3);
+		vertexBuffer.put(p.getXf()).put(p.getYf()).put(p.getZf());
+		vertexBuffer.put(p.getXf()).put(p.getYf()).put(WALL_HEIGHT);
+
+//		if (i == 0) {
+//			vertexBuffer.put(p.getXf()).put(p.getYf()).put(p.getZf());
+//			vertexBuffer.put(p.getXf()).put(p.getYf()).put(WALL_HEIGHT);
+//		}		
+		
+		final float TEXTURE_SCALE = (i < 1) ? 0 : (float)p.subtract(points.get(i-1), null).length();
+
+		// texture coords
+		textureBuffer.position(i * 2 * 2);
+		textureBuffer.put(TEXTURE_SCALE).put(0);
+		textureBuffer.put(TEXTURE_SCALE).put(1);
+	}
+
+}
