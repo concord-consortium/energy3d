9ea65de3d10724d9d0b2ac8c616437fffc1ca018
snourian <snourian@6e01202a-0783-4428-890a-84243c50cc2b>
2010-09-09 20:12:15 +0000

    - fix problem with camera distortion when reseting the view due to missing frustum calls in resetCamera
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/simulations/Energy3D@23212 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/energy3d/scene/SceneManager.java
+++ src/org/concord/energy3d/scene/SceneManager.java
@@ -284,7 +284,7 @@ public class SceneManager implements com.ardor3d.framework.Scene, Runnable, Upda
 		frameHandler.updateFrame();
 		resetCamera(ViewMode.NORMAL);
 //		canvas.getCanvasRenderer().getCamera().setFrustumPerspective(45.0, 16 / 10.0, 1, 1000);
-		canvas.getCanvasRenderer().getCamera().setFrustumPerspective(45.0, 16 / 10.0, 0.01, 200);
+////		canvas.getCanvasRenderer().getCamera().setFrustumPerspective(45.0, 16 / 10.0, 0.01, 200);
 
 //		compass.addController(new SpatialController<Spatial>() {
 //			public void update(double time, Spatial caller) {
@@ -823,6 +823,8 @@ public class SceneManager implements com.ardor3d.framework.Scene, Runnable, Upda
 			resizeCamera(camera);
 		cameraNode.updateFromCamera();
 
+//		canvas.getCanvasRenderer().getCamera().setFrustumPerspective(45.0, 16 / 10.0, 1, 1000);
+		canvas.getCanvasRenderer().getCamera().setFrustumPerspective(45.0, 16 / 10.0, 0.01, 200);		
 	}
 
 	// public void topCameraView() {
