74ff959e8cb01a4d2b6a483a00048e3397504a59
snourian <snourian@6e01202a-0783-4428-890a-84243c50cc2b>
2011-03-14 21:00:21 +0000

    debug
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/simulations/Energy3D@24372 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/energy3d/util/MeshLib.java
+++ src/org/concord/energy3d/util/MeshLib.java
@@ -3,6 +3,7 @@ package org.concord.energy3d.util;
 import java.nio.FloatBuffer;
 import java.util.ArrayList;
 
+import com.ardor3d.math.Matrix3;
 import com.ardor3d.math.Vector2;
 import com.ardor3d.math.Vector3;
 import com.ardor3d.scenegraph.Mesh;
@@ -11,14 +12,20 @@ import com.ardor3d.scenegraph.hint.CullHint;
 import com.ardor3d.util.geom.BufferUtils;
 
 public class MeshLib {
+	private static class GroupData {
+		final Vector3 key = new Vector3();
+		final ArrayList<Vector3> vertices = new ArrayList<Vector3>();
+		final ArrayList<Vector3> normals = new ArrayList<Vector3>();
+		final ArrayList<Vector2> textures = new ArrayList<Vector2>();
+	}
 	
 	public static void groupByPlanner(final Mesh mesh, final Node root) {
-		class GroupData {
-			final Vector3 key = new Vector3();
-			final ArrayList<Vector3> vertices = new ArrayList<Vector3>();
-			final ArrayList<Vector3> normals = new ArrayList<Vector3>();
-			final ArrayList<Vector2> textures = new ArrayList<Vector2>();
-		}
+		final ArrayList<GroupData> groups = extractGroups(mesh);
+		computeHorizontalTextureCoords(groups);
+		createMeshes(root, groups);
+	}
+	
+	public static ArrayList<GroupData> extractGroups(final Mesh mesh) {
 		final FloatBuffer vertexBuffer = mesh.getMeshData().getVertexBuffer();
 		final FloatBuffer normalBuffer = mesh.getMeshData().getNormalBuffer();
 		final FloatBuffer textureBuffer = mesh.getMeshData().getTextureBuffer(0);
@@ -61,7 +68,50 @@ public class MeshLib {
 			group.textures.add(new Vector2(textureBuffer.get(), textureBuffer.get()));
 			group.textures.add(new Vector2(textureBuffer.get(), textureBuffer.get()));
 		}
-		
+		return groups;
+	}
+	
+	private static void computeHorizontalTextureCoords(ArrayList<GroupData> groups) {
+		for (final GroupData group : groups) {
+			int topIndex = 0;
+			for (int i = 1; i < group.vertices.size(); i++)
+				if (group.vertices.get(i).getZ() > group.vertices.get(topIndex).getZ())
+					topIndex = i;
+			final Vector3 normal = group.normals.get(0);
+			final double angle = normal.smallestAngleBetween(Vector3.UNIT_Y);
+			final Vector3 rotAxis = normal.cross(Vector3.UNIT_Y, null);
+			final Matrix3 matrix = new Matrix3().fromAngleAxis(angle, rotAxis);
+			final Vector3 base = new Vector3(group.vertices.get(topIndex == 2 ? 1 : 0));
+//			base.su
+				
+			
+			final Vector3 min = new Vector3(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+			final Vector3 max = new Vector3(Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE);
+			for (final Vector3 p : group.vertices) {
+				min.set(Math.min(min.getX(), p.getX()), Math.min(min.getY(), p.getY()), Math.min(min.getZ(), p.getZ()));
+				max.set(Math.max(max.getX(), p.getX()), Math.max(max.getY(), p.getY()), Math.max(max.getZ(), p.getZ()));
+			}
+			final double maxV = max.getZ() - min.getZ();
+			min.setZ(0);
+			max.setZ(0);
+			final double maxU = min.distance(max);
+			System.out.println("---");
+			for (int i = 0; i < group.vertices.size(); i++) {
+				final Vector3 p = group.vertices.get(i).clone();
+//				System.out.println(p);
+				matrix.applyPost(p, p);
+//				System.out.println(p);
+//				p.subtractLocal(min);
+				final double v = p.getZ();
+//				p.setZ(0);
+				final double u = p.getX();
+				group.textures.get(i).set(u, v);
+				System.out.println(u + ", " + v + "\t\t\t" + p);
+			}
+		}
+	}
+
+	public static void createMeshes(final Node root, final ArrayList<GroupData> groups) {
 		int meshIndex = 0;
 		for (final GroupData group : groups) {
 			final Mesh newMesh;
@@ -115,5 +165,7 @@ public class MeshLib {
 			root.getChild(meshIndex).getSceneHints().setCullHint(CullHint.Always);
 			meshIndex++;
 		}
-	}	
+	}
+
+	
 }
