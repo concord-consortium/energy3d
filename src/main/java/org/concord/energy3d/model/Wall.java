b002a6eedccc2296b72a68d118386e51f6256f5e
Saeid Nourian <snourian@concord.org>
2013-09-12 16:27:23 -0400

    - open models faster by avoiding redrawing after each fixDisconnectWall call

--- src/main/java/org/concord/energy3d/model/Wall.java
+++ src/main/java/org/concord/energy3d/model/Wall.java
@@ -1173,7 +1173,8 @@ public class Wall extends HousePart {
 							if ((otherWall.neighbors[otherIndex] == null || otherWall.neighbors[otherIndex].getNeighborOf(otherWall) == this) && otherWall.getAbsPoint(otherIndex * 2).distance(getAbsPoint(index * 2)) < MathUtils.ZERO_TOLERANCE) {
 								System.out.println("Fixing neighbor...");
 								setNeighbor(index * 2, new Snap(this, otherWall, index * 2, otherIndex * 2), true);
-								drawNeighborWalls();
+//								drawNeighborWalls();
+								Scene.getInstance().redrawAll();
 							}
 						}
 			}
