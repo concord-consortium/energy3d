57ab19e540bfe1461282466b500c67bc707a0c9c
charxie <charxie@gmail.com>
2014-02-22 10:17:01 -0500

    .

---
+++ src/main/java/org/concord/energy3d/gui/IrradiationParametersDialog.java
@@ -0,0 +1,79 @@
+package org.concord.energy3d.gui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import org.concord.energy3d.gui.EnergyPanel.UpdateRadiation;
+
+class IrradiationParametersDialog extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+	private final static DecimalFormat FORMAT1 = new DecimalFormat("#0.##");
+
+	public IrradiationParametersDialog() {
+
+		super(MainFrame.getInstance(), true);
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setTitle("Solar Irradiation");
+
+		getContentPane().setLayout(new BorderLayout());
+		JPanel contentPanel = new JPanel();
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+
+		contentPanel.add(new JLabel("Simulation Resolution"));
+
+		final JTextField resolutionTextField = new JTextField(FORMAT1.format(EnergyPanel.getInstance().getSolarStep()));
+		contentPanel.add(resolutionTextField);
+		resolutionTextField.setColumns(20);
+
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+
+		JButton okButton = new JButton("OK");
+		okButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				try {
+					double resolution = Double.parseDouble(resolutionTextField.getText());
+					IrradiationParametersDialog.this.dispose();
+					EnergyPanel.getInstance().setSolarStep(resolution);
+					EnergyPanel.getInstance().compute(UpdateRadiation.ONLY_IF_SLECTED_IN_GUI);
+				} catch (final NumberFormatException err) {
+					err.printStackTrace();
+					JOptionPane.showMessageDialog(IrradiationParametersDialog.this, "Invalid input: " + err.getMessage(), "Invalid Input", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+		okButton.setActionCommand("OK");
+		buttonPanel.add(okButton);
+		getRootPane().setDefaultButton(okButton);
+
+		final JButton cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				IrradiationParametersDialog.this.dispose();
+			}
+		});
+		cancelButton.setActionCommand("Cancel");
+		buttonPanel.add(cancelButton);
+
+		pack();
+		setLocationRelativeTo(MainFrame.getInstance());
+
+	}
+
+}
