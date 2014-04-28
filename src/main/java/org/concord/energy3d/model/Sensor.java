a2d07b845cf8ba0a4e44801fc16725a6f788d9c2
charxie <charxie@gmail.com>
2014-04-28 15:02:31 -0400

    Sensor on top of window or door

--- src/main/java/org/concord/energy3d/model/Sensor.java
+++ src/main/java/org/concord/energy3d/model/Sensor.java
@@ -68,7 +68,8 @@ public class Sensor extends HousePart {
 
 	@Override
 	public void setPreviewPoint(final int x, final int y) {
-		final PickedHousePart picked = pickContainer(x, y, new Class<?>[] { Roof.class, Wall.class });
+		final PickedHousePart picked = pickContainer(x, y, new Class<?>[] { Roof.class, Wall.class});
+		// final PickedHousePart picked = pickContainer(x, y, new Class<?>[] { Roof.class, Wall.class, Foundation.class });
 		if (picked != null) {
 			final Vector3 p = picked.getPoint();
 			if (container instanceof Wall)
