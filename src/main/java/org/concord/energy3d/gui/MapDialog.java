000784e086e9f496f5682b9bdc4b176ed4ad541d
Saeid Nourian <snourian@concord.org>
2016-08-17 13:19:42 -0400

    Set default map view to globe Allow moving map via mouse drag Allow zooming in the map via mouse wheel

--- src/main/java/org/concord/energy3d/gui/MapDialog.java
+++ src/main/java/org/concord/energy3d/gui/MapDialog.java
@@ -1,7 +1,12 @@
 package org.concord.energy3d.gui;
 
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
@@ -26,15 +31,18 @@ import org.concord.energy3d.scene.Scene;
 
 import com.ardor3d.image.Image;
 import com.ardor3d.image.util.awt.AWTImageLoader;
+import com.ardor3d.math.MathUtils;
 
 public class MapDialog extends JDialog {
 	private static final long serialVersionUID = 1L;
-	private static MapDialog instance;
+	private static final int zoomMin = 0;
+	private static final int zoomMax = 21;
 	private final JTextField addressField = new JTextField("25 Love lane, Concord, MA, USA", 30);
 	private final JSpinner latitudeSpinner = new JSpinner(new SpinnerNumberModel(42.45661, -90, 90, 0.00001));
 	private final JSpinner longitudeSpinner = new JSpinner(new SpinnerNumberModel(-71.35823, -90, 90, 0.00001));
-	private final JSpinner zoomSpinner = new JSpinner(new SpinnerNumberModel(20, 17, 21, 1));
+	private final JSpinner zoomSpinner = new JSpinner(new SpinnerNumberModel(2, zoomMin, zoomMax, 1));
 	private final JLabel mapLabel = new JLabel();
+	private static MapDialog instance;
 	private boolean lock = false;
 
 	public static MapDialog getInstance() {
@@ -47,9 +55,48 @@ public class MapDialog extends JDialog {
 	private MapDialog(final JFrame owner) {
 		super(owner);
 		setTitle("Map");
-		mapLabel.setAlignmentX(0.5f);
 		latitudeSpinner.setEditor(new JSpinner.NumberEditor(latitudeSpinner, "0.00000"));
 		longitudeSpinner.setEditor(new JSpinner.NumberEditor(longitudeSpinner, "0.00000"));
+		mapLabel.setAlignmentX(0.5f);
+		final Point point = new Point();
+		mapLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+			@Override
+			public void mousePressed(final MouseEvent e) {
+				point.setLocation(e.getPoint());
+			}
+
+			@Override
+			public void mouseReleased(final MouseEvent e) {
+				point.setLocation(0, 0);
+			}
+		});
+		mapLabel.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(final MouseEvent e) {
+				if (point.getX() != 0 && point.getY() != 0) {
+					final double dx = e.getX() - point.getX();
+					final double dy = e.getY() - point.getY();
+					final Double lat = (Double) latitudeSpinner.getValue();
+					final Double lng = (Double) longitudeSpinner.getValue();
+					lock = true;
+					final double scale = getScale();
+					latitudeSpinner.setValue(lat + dy / 1000000.0 * scale);
+					longitudeSpinner.setValue(lng - dx / 750000.0 * scale);
+					lock = false;
+					updateMap();
+				}
+				point.setLocation(e.getPoint());
+			}
+		});
+		mapLabel.addMouseWheelListener(new MouseWheelListener() {
+			@Override
+			public void mouseWheelMoved(final MouseWheelEvent e) {
+				lock = true;
+				zoomSpinner.setValue(MathUtils.clamp((Integer) zoomSpinner.getValue() - e.getWheelRotation(), zoomMin, zoomMax));
+				lock = false;
+				updateMap();
+			}
+		});
 		addressField.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(final ActionEvent e) {
@@ -58,6 +105,7 @@ public class MapDialog extends JDialog {
 					lock = true;
 					latitudeSpinner.setValue(coordinates[0]);
 					longitudeSpinner.setValue(coordinates[1]);
+					zoomSpinner.setValue(20);
 					lock = false;
 					updateMap();
 				}
@@ -94,19 +142,14 @@ public class MapDialog extends JDialog {
 		okButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(final ActionEvent e) {
+				if ((Integer) zoomSpinner.getValue() < 17) {
+					JOptionPane.showMessageDialog(MapDialog.this, "The selected region is too large. Please zoom in and try again.", MapDialog.this.getTitle(), JOptionPane.WARNING_MESSAGE);
+					return;
+				}
 				final BufferedImage googleMapImage = getGoogleMapImage(true);
 				if (googleMapImage != null) {
 					final Image image = AWTImageLoader.makeArdor3dImage(googleMapImage, true);
-					final int zoom = (Integer) zoomSpinner.getValue();
-					final double scale;
-					if (zoom == 21) {
-						scale = 0.5;
-					} else if (zoom == 20) {
-						scale = 1;
-					} else {
-						scale = (Math.pow(2, 20 - zoom));
-					}
-					Scene.getInstance().setMap(image, scale);
+					Scene.getInstance().setMap(image, getScale());
 					Scene.getInstance().setEdited(true);
 					setVisible(false);
 				}
@@ -177,5 +220,18 @@ public class MapDialog extends JDialog {
 		}
 		JOptionPane.showMessageDialog(this, "Could not find the address!", "Error", JOptionPane.WARNING_MESSAGE);
 		return null;
+	}
+
+	private double getScale() {
+		final int zoom = (Integer) zoomSpinner.getValue();
+		final double scale;
+		if (zoom == 21) {
+			scale = 0.5;
+		} else if (zoom == 20) {
+			scale = 1;
+		} else {
+			scale = (Math.pow(2, 20 - zoom));
+		}
+		return scale;
 	}
 }
