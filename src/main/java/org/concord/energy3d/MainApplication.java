e87514d88e56bef064ffd335ebf8c5589809bd6d
Charles Xie <charxie@gmail.com>
2016-10-02 20:52:30 -0400

    Solar panels stay on the rack when base height is lower than half of the height.

--- src/main/java/org/concord/energy3d/MainApplication.java
+++ src/main/java/org/concord/energy3d/MainApplication.java
@@ -21,7 +21,7 @@ import org.concord.energy3d.util.Updater;
 
 public class MainApplication {
 
-	public static final String VERSION = "5.9.4";
+	public static final String VERSION = "5.9.5";
 
 	public static boolean appDirectoryWritable = true;
 	public static boolean isMacOpeningFile;
