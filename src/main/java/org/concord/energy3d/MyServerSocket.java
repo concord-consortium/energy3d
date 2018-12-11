c0bc3c34f63c6492580ae84057941f117cece772
Charles Xie <charxie@gmail.com>
2018-12-10 20:19:31 -0500

    server socket work

--- src/main/java/org/concord/energy3d/MyServerSocket.java
+++ src/main/java/org/concord/energy3d/MyServerSocket.java
@@ -3,6 +3,7 @@ package org.concord.energy3d;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -26,7 +27,7 @@ public class MyServerSocket {
 		}
 	}
 
-	public void listen() throws Exception {
+	public void listen1() throws Exception {
 		client = server.accept();
 		final String clientAddress = client.getInetAddress().getHostAddress();
 		System.out.println("\r\nConnection from " + clientAddress);
@@ -38,6 +39,24 @@ public class MyServerSocket {
 			System.out.println("\r\nMessage from " + clientAddress + ": " + data);
 			out.println("B\n");
 			out.flush();
+		}
+	}
+
+	public void listen() throws Exception {
+		client = server.accept();
+		final String clientAddress = client.getInetAddress().getHostAddress();
+		System.out.println("\r\nConnection from " + clientAddress);
+		final OutputStream out = client.getOutputStream();
+		final byte[] bytes = new byte[3];
+		bytes[0] = 11;
+		bytes[1] = 22;
+		bytes[2] = 33;
+		out.write(bytes);
+		out.flush();
+		final InputStream in = client.getInputStream();
+		int x;
+		while ((x = in.read()) != -1) {
+			System.out.println("receiving " + x);
 		}
 	}
 
