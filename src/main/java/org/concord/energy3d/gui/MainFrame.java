3defd6d943a992abcb032bcff0e38c2553c0d468
Charles Xie <charxie@gmail.com>
2019-09-16 08:05:35 -0400

    new example

--- src/main/java/org/concord/energy3d/gui/MainFrame.java
+++ src/main/java/org/concord/energy3d/gui/MainFrame.java
@@ -1368,6 +1368,7 @@ public class MainFrame extends JFrame {
             addModel(pvSolarMenu, "Solar Facades: Example 1", "templates/example-solar-facade1.ng3");
             addModel(pvSolarMenu, "Solar Facades: Example 2", "templates/example-solar-facade2.ng3");
             addModel(pvSolarMenu, "Solar Facades: Example 3", "templates/example-solar-facade3.ng3");
+            addModel(pvSolarMenu, "Solar Facades: Example 4", "templates/example-solar-facade4.ng3");
             addModel(pvSolarMenu, "Agrivoltaics: Example 1", "templates/example-agrivoltaics-01.ng3");
             examplesMenu.add(pvSolarMenu);
             final JMenu cspSolarMenu = new JMenu("Concentrated Solar Power Systems");
